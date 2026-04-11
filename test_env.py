298df407b0218fed8956f47ed08910e6ea452568
aidankimberley <aidanlkimberley@gmail.com>
2026-04-11 17:01:20 -0400

    initial commit

---
+++ test_env.py
@@ -0,0 +1,16 @@
+import gymnasium as gym
+import gymnasium_robotics
+
+gym.register_envs(gymnasium_robotics)
+
+env = gym.make("AntMaze_UMaze-v5", render_mode="human", max_episode_steps=100)
+env.reset()
+for i in range(5000):
+    action = env.action_space.sample()
+
+    obs, reward, terminated, truncated, info = env.step(action)
+    print(f"Step {i}: Reward = {reward}, Terminated = {terminated}, Truncated = {truncated}")   
+    if terminated or truncated:
+        break
+
+env.close()
